24aec297290fa6d56355c3eb728c937ff8f22761
MotasemZiad <motasemabunima@gmail.com>
2023-05-04 13:10:33 +0300

    some updates

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -23,8 +23,8 @@ class SendChirpCreatedNotifications implements ShouldQueue
      */
     public function handle(ChirpCreated $event): void
     {
-        foreach(User::whereNot('id', $event->chirp->user_id)->cursor() as $user){
-            $user->notify(new NewChirp($event->chirp));
-        }
+        // foreach(User::whereNot('id', $event->chirp->user_id)->cursor() as $user){
+        //     $user->notify(new NewChirp($event->chirp));
+        // }
     }
 }
